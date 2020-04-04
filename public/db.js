5ee173954373e5717ebfddae7de361e53344418c
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-04-04 14:56:56 -0400

    offline functionality working

--- public/db.js
+++ public/db.js
@@ -11,6 +11,17 @@ request.onerror = function (e) {
   console.log("Woops! " + e.target.errorCode);
 };
 
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+
+  // add record to your store with add method.
+  store.add(record);
+}
+
 request.onsuccess = function(e) {
   db = e.target.result;
 
